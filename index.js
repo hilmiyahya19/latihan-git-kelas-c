352e093d489ab96d5877f41e88f7de63eab1d193
hilmiyahya19 <hilmiyahya199@gmail.com>
2023-09-26 11:32:36 +0700

    pertemuan selasa 26-9-23

---
+++ index.js
@@ -0,0 +1,52 @@
+document.write("<hr>");
+document.write("Hello we're studying basics java script");
+document.write("<hr>");
+
+// alert("this is a private website");
+// confirm("are you sure?");
+// prompt("what can i help you?");
+
+// let message;
+// message = 'Hello';
+
+// alert(message)
+
+function test() {
+    let message = 'Hello';
+    if(true){
+        let message = 'email';
+    }
+    console.log(message);
+}
+
+test()
+
+let $ = 2;
+let _ = 3;
+
+document.write($+_)
+
+const myBirthday = '01.01.2000';
+// myBirthday = '02.02.2000';
+console.log(myBirthday);
+
+// let user = "Hilmi Yahya";
+// let age = 20;
+// let message ="Hello guys";
+
+// console.log(user,age,message);
+
+// let user = "Hilmi Yahya";
+// let age = 20;
+// let message ="Hello guys";
+
+// alert(user+' '+age+' '+message);
+
+document.write("<hr>");
+
+let user = "Hilmi Yahya";
+let age = 20;
+let message ="Hello guys";
+
+document.write(user+" "+age+" "+message);
+
