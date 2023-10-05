ee2740330ea8b1d2b558f20bc2e865dfb4c25d4c
hilmiyahya19 <hilmiyahya199@gmail.com>
2023-10-05 11:22:49 +0700

    pertemuan rabu 4 oktober 2023

--- index.js
+++ index.js
@@ -1,52 +1,147 @@
-document.write("<hr>");
-document.write("Hello we're studying basics java script");
-document.write("<hr>");
+// document.write("<hr>");
+// document.write("Hello we're studying basics java script");
+// document.write("<hr>");
 
-// alert("this is a private website");
-// confirm("are you sure?");
-// prompt("what can i help you?");
+// // alert("this is a private website");
+// // confirm("are you sure?");
+// // prompt("what can i help you?");
 
-// let message;
-// message = 'Hello';
+// // let message;
+// // message = 'Hello';
 
-// alert(message)
+// // alert(message)
 
-function test() {
-    let message = 'Hello';
-    if(true){
-        let message = 'email';
-    }
-    console.log(message);
-}
+// function test() {
+//     let message = 'Hello';
+//     if(true){
+//         let message = 'email';
+//     }
+//     console.log(message);
+// }
 
-test()
+// test()
 
-let $ = 2;
-let _ = 3;
+// let $ = 2;
+// let _ = 3;
 
-document.write($+_)
+// document.write($+_)
 
-const myBirthday = '01.01.2000';
-// myBirthday = '02.02.2000';
-console.log(myBirthday);
+// const myBirthday = '01.01.2000';
+// // myBirthday = '02.02.2000';
+// console.log(myBirthday);
+
+// // let user = "Hilmi Yahya";
+// // let age = 20;
+// // let message ="Hello guys";
+
+// // console.log(user,age,message);
+
+// // let user = "Hilmi Yahya";
+// // let age = 20;
+// // let message ="Hello guys";
+
+// // alert(user+' '+age+' '+message);
+
+// document.write("<hr>");
 
 // let user = "Hilmi Yahya";
 // let age = 20;
 // let message ="Hello guys";
 
-// console.log(user,age,message);
+// document.write(user+" "+age+" "+message);
 
-// let user = "Hilmi Yahya";
-// let age = 20;
-// let message ="Hello guys";
+// // rabu 27 september 2023
 
-// alert(user+' '+age+' '+message);
+// let x;
+// console.log(typeof(x));
 
-document.write("<hr>");
+// let v = null;
+// console.log(typeof(v));
 
-let user = "Hilmi Yahya";
-let age = 20;
-let message ="Hello guys";
+// let plus = 2+3
+// let min = 7-2
+// let miring = 5/2
+// let bintang = 5*3
+// let modulus = 10 % 3
 
-document.write(user+" "+age+" "+message);
+
+// console.log(plus);
+// console.log(miring);
+// console.log(bintang);
+// console.log(modulus);
+// console.log(min);
+
+// let hitung = 1999999999999999999999999n
+// console.log(hitung);
+
+// console.log(100n + 200n);
+
+// let hi = `hello
+// world`;
+
+// console.log(hi);
+
+// let hai = "hello\nworld\n";
+
+// console.log(hai);
+
+// let nama = "Hilmi Yahya"
+// let alamat = "kediri"
+// let umur = "20"
+
+// // console.log(nama);
+// // console.log(alamat);
+// // console.log(umur);
+
+// let detail = nama + " " + alamat +" "+ umur
+// console.log(detail);
+
+// let array1 = ["Hilmi", 20, true]
+// console.log(array1);
+// console.log(array1[0]);
+// console.log(array1[2]);
+
+// let array1 = ["Hilmi", 20, true]
+// let hapus = array1.pop()
+
+// console.log(array1);
+
+// let tes = "10" + 10
+// console.log(tes);
+
+// belajar push , length
+
+// rabu 4 oktober 2023
+
+// let z = 1
+// switch (z) {
+//     case 1:
+//         console.log("ini satu");
+//         break;
+//     case 2:
+//         console.log("ini dua");
+//         break;
+//     default:
+//         console.log("ini gatau");
+// }
+
+
+
+// function myFunction() {
+//     const message = document.getElementById("p01"); 
+//     message.innerHTML = "";
+//     let x = document.getElementById("demo").value; 
+//     try {
+//         if(x.trim() == "") throw "empty"; 
+//         if(isNaN(x)) throw "not a number";
+//         X = Number(x);
+//         if(x < 5) throw "too low"; 
+//         if(x > 10) throw "too high";
+//     }
+//     catch(err) {
+//         message.innerHTML = "Input is " + err;
+//     }
+// }
+
+// kamis 5 oktober 2023
 
